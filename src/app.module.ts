4fa5c850b301a7705a1f4b58ce2ea25fd4dc9620
bal16 <ahmadhamidbalya@gmail.com>
2025-10-28 18:17:55 +0700

    feat: initialize NestJS application with Prisma and basic structure
    - Add Prisma configuration file for database connection.
    - Create AppController and AppService with a simple "Hello World!" endpoint.
    - Set up AppModule to include necessary modules and configuration.
    - Implement main entry point for the application.
    - Add unit tests for AppController.
    - Create end-to-end tests for the application.
    - Configure Jest for end-to-end testing.
    - Set up TypeScript configuration for building the project.

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './infra/database/pirsma.module';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PrismaModule,
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
