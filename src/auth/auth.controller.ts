6af7115e79b17fc106eebe67b680930711d90dc4
bal16 <ahmadhamidbalya@gmail.com>
2025-11-24 14:25:07 +0700

    refactor: enhance AuthController to return standardized WebResponse

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,11 +9,20 @@ import {
   UseGuards,
   Delete,
 } from '@nestjs/common';
+
 import { AuthService } from './auth.service';
+
 import { LoginDTO } from './dto/login.dto';
 import { RegistrationDTO } from './dto/registeration.dto';
+
+import { WebResponse } from '../common/responses/web.response';
+import { LoginUser } from './entities/login.entity';
+import { RegisteredUser } from './entities/register.entity';
+import { CurrentUser } from './entities/current.entity';
+
 import { RefreshTokenGuard } from './strategies/refreshToken.guard';
 import { AccessTokenGuard } from './strategies/accessToken.guard';
+
 import type { CustomRequest } from '../type';
 
 @Controller('auth')
@@ -22,43 +31,63 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  login(@Body() LoginDTO: LoginDTO) {
-    return this.authService.authenticate(LoginDTO);
+  async login(@Body() LoginDTO: LoginDTO): Promise<WebResponse<LoginUser>> {
+    const user = await this.authService.signIn(LoginDTO);
+
+    return new WebResponse('Success', user, HttpStatus.OK);
   }
 
   @HttpCode(HttpStatus.CREATED)
   @Post('register')
-  async register(@Body() RegistrationDTO: RegistrationDTO) {
-    return await this.authService.register(RegistrationDTO);
+  async register(
+    @Body() RegistrationDTO: RegistrationDTO,
+  ): Promise<WebResponse<RegisteredUser>> {
+    const user = await this.authService.register(RegistrationDTO);
+
+    return new WebResponse(
+      'User successfully registered',
+      user,
+      HttpStatus.CREATED,
+    );
   }
 
   @UseGuards(RefreshTokenGuard)
   @HttpCode(HttpStatus.OK)
   @Get('refresh')
-  async refresh(@Req() request: CustomRequest) {
+  async refresh(
+    @Req() request: CustomRequest,
+  ): Promise<WebResponse<LoginUser>> {
     const authorization = request.headers.authorization!;
     const token = authorization.split(' ')[1];
 
     const { email } = request.user;
 
-    return await this.authService.refresh(token, email);
+    const user = await this.authService.refresh(token, email);
+
+    return new WebResponse('Success', user, HttpStatus.OK);
   }
 
   @UseGuards(AccessTokenGuard)
   @HttpCode(HttpStatus.OK)
   @Get('me')
-  async getMe(@Req() request: CustomRequest) {
+  async getMe(
+    @Req() request: CustomRequest,
+  ): Promise<WebResponse<CurrentUser>> {
     const { email } = request.user;
 
-    return await this.authService.getSession(email);
+    const user = await this.authService.getSession(email);
+
+    return new WebResponse('Success', user, HttpStatus.OK);
   }
 
   @UseGuards(AccessTokenGuard)
   @HttpCode(HttpStatus.OK)
   @Delete('session')
-  async logout(@Req() request: CustomRequest) {
+  async logout(@Req() request: CustomRequest): Promise<WebResponse<null>> {
     const { sub } = request.user;
 
-    return await this.authService.logout(sub);
+    await this.authService.logout(sub);
+
+    return new WebResponse('Success', null, HttpStatus.OK);
   }
 }
