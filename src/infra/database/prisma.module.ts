c89c2fe35571aee37035e865c5b6d8c15a3b419e
bal16 <ahmadhamidbalya@gmail.com>
2025-12-15 13:52:02 +0700

    refactor: reorganize Prisma configuration and service for improved structure and clarity

--- src/infra/database/prisma.module.ts
+++ src/infra/database/prisma.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
+  // With ConfigModule set isGlobal: true in AppModule, no import needed here
   providers: [PrismaService],
   exports: [PrismaService],
 })
--- src/infra/database/prisma.service.ts
+++ src/infra/database/prisma.service.ts
@@ -1,9 +1,15 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../../../generated/prisma';
+import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
+export class PrismaService extends PrismaClient {
+  constructor(private readonly configService: ConfigService) {
+    const adapter = new PrismaPg({
+      connectionString: configService.get<string>('DATABASE_URL'),
+    });
+
+    super({ adapter });
   }
 }
