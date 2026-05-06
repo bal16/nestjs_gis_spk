41b1e4c4efce626589c6116dded525844b61db72
bal16 <ahmadhamidbalya@gmail.com>
2026-05-06 12:01:30 +0700

    refactor: remove unnecessary comment in PrismaModule

--- src/infra/database/prisma.module.ts
+++ src/infra/database/prisma.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  // With ConfigModule set isGlobal: true in AppModule, no import needed here
   providers: [PrismaService],
   exports: [PrismaService],
 })
