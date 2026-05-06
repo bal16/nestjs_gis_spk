a7177a7ffd3335a903200f3439eb5bce1a381bd6
bal16 <ahmadhamidbalya@gmail.com>
2026-05-06 11:59:55 +0700

    refactor: update LoginUser property names to camelCase

--- src/auth/entities/login.entity.ts
+++ src/auth/entities/login.entity.ts
@@ -1,8 +1,9 @@
 export class LoginUser {
   constructor(
-    public readonly access_token: string,
-    public readonly refresh_token: string,
+    public readonly accessToken: string,
+    public readonly refreshToken: string,
     public readonly username: string,
+    public readonly isAdmin: boolean,
     public readonly id: string,
   ) {}
 }
