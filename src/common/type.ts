ab90004187173c455fd990ba5645c734cbce5f89
bal16 <ahmadhamidbalya@gmail.com>
2026-05-06 12:00:36 +0700

    refactor: add Prisma import and update BuildingWithAssessment type definition

--- src/common/type.ts
+++ src/common/type.ts
@@ -1,6 +1,13 @@
 import type { Request } from 'express';
 import type { JwtPayload } from '../auth/dto/jwt.dto';
+import type { Prisma } from 'src/generated/prisma/client';
+// import { Prisma } from 'src/generated/prisma';
 
 export interface CustomRequest extends Request {
   user: JwtPayload;
 }
+
+// Gunakan tipe ini di Service
+export type BuildingWithAssessment = Prisma.BuildingGetPayload<{
+  include: { assessments: true };
+}>;
